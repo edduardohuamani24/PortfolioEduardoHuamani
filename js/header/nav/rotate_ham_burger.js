01e219f45e5c1a9523283d172d26a63182eabab7
edduardohuamani24 <eduardoHuamaniC2019@gmail.com>
2021-07-03 00:23:57 -0500

    Se terminó el desarrollo de la vista en responsive del nav. Se creó otro archivo js para darle un cambio a las barras del botón hamburger y convertirlo en el símbolo x de cerrar.

---
+++ js/header/nav/rotate_ham_burger.js
@@ -0,0 +1,15 @@
+/*Creamos una constante para almacenar los span del button hamburger*/
+const button_br = document.querySelectorAll(".button-hamburger span");
+
+const rotateBar = () => {
+  /*Usamos el método forEach para que recorra los 3 span contenidos en la constante button_br*/
+  button_br.forEach((child) => {
+    /*
+    A esta función anónima se le pone un párametro que lo llamaremos child
+    Mediante este parámetro child accedemos a la propiedad classList y con el método toggle agregamos o quitamos la clase .animation
+    */
+    child.classList.toggle("animation");
+  });
+};
+
+export { rotateBar };
--- js/header/nav/show_nav_list_responsive.js
+++ js/header/nav/show_nav_list_responsive.js
@@ -1,10 +1,15 @@
+import { rotateBar } from "./rotate_ham_burger.js";
 const button_ham = document.querySelector(".button-hamburger");
 const nav_list = document.querySelector(".nav__list");
 
-export const openCloseHam = () => {
+const openCloseHam = () => {
   //El botón hamburguesa se le agregá un evento list que es una función anónimma.
   button_ham.addEventListener("click", () => {
     /*Mediante el método toggle se agregará la clase .nav__list-activate al nav__list en caso no tenga y si tiene la clase, lo quitará.  */
     nav_list.classList.toggle("nav__list-activate");
+    /*Se agrega la función para rotar la barras del botón hamburger*/
+    rotateBar();
   });
 };
+
+export { openCloseHam };
