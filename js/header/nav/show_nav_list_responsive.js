135f80e2ebd856bad8c9d1157e83fb283566829e
edduardohuamani24 <eduardoHuamaniC2019@gmail.com>
2021-07-03 21:24:52 -0500

    La vista de la pantalla de inicio para desktop está completa. Sin embargo, no se descarta posibles actualizaciones.

--- js/header/nav/show_nav_list_responsive.js
+++ js/header/nav/show_nav_list_responsive.js
@@ -6,7 +6,7 @@ const openCloseHam = () => {
   //El botón hamburguesa se le agregá un evento list que es una función anónimma.
   button_ham.addEventListener("click", () => {
     /*Mediante el método toggle se agregará la clase .nav__list-activate al nav__list en caso no tenga y si tiene la clase, lo quitará.  */
-    nav_list.classList.toggle("nav__list-activate");
+    nav_list.classList.toggle("nav__list--activate");
     /*Se agrega la función para rotar la barras del botón hamburger*/
     rotateBar();
   });
