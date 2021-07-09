00fdc1f5ad7c2d7f792feb02cf276f71edd8dc9a
edduardohuamani24 <eduardoHuamaniC2019@gmail.com>
2021-07-09 00:43:30 -0500

    Se realizó el contenido de las cards. Falta corregis el tipo de medida y la vista para móviles.

--- js/main/projects/card/rotate_img.js
+++ js/main/projects/card/rotate_img.js
@@ -6,12 +6,25 @@ const rotateImage = () => {
   Se crea una función anónima con el parámetro element
   */
   card_item.forEach(function (element) {
-    /* Mediante el parámetro element accedemos al método addEventListener para que con evento click  se ejecute otra función anónima */
-    element.addEventListener("click", function () {
+    /* Mediante el parámetro element accedemos al método addEventListener para que con el evento mouseover al pasar el cursor en el elemento card se ejecute una función anónima*/
+    element.addEventListener("mouseover", function () {
       /*this: Reemplaza al parámetro.
-      Accedemos a la propiedad classList y con el método toggle agregamos o quitamos la clase .rotate-img */
-      this.classList.toggle("rotate-img");
+      Accedemos a la propiedad classList y con el método add agregamos la clase .rotate-img */
+      this.classList.add("rotate-img");
     });
+    /* En este caso usamos el parámetro element para usar el método addEventListener, que con el evento mouseout al retirar el cursor del elemento card se ejecute una función anónima*/
+    element.addEventListener("mouseout", function () {
+      /*this: Reemplaza al parámetro.
+      Accedemos a la propiedad classList y con el remove quitamos la clase .rotate-img */
+      this.classList.remove("rotate-img");
+    });
+    /*** MEDIANTE CLICK ***/
+    /* Mediante el parámetro element accedemos al método addEventListener para que con evento click  se ejecute otra función anónima */
+    // element.addEventListener("click", function () {
+    //   /*this: Reemplaza al parámetro.
+    //   Accedemos a la propiedad classList y con el método toggle agregamos o quitamos la clase .rotate-img */
+    //   this.classList.toggle("rotate-img");
+    // });
   });
 };
 
