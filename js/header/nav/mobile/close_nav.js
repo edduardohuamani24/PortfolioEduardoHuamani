5048fdd82b4c6427ec6b3515fee7ad750684238d
edduardohuamani24 <eduardoHuamaniC2019@gmail.com>
2021-07-27 00:25:31 -0500

    Se creó el método para cerrar la ventana de menu en vista para móviles. Aún hay errores.

--- js/header/nav/mobile/close_nav.js
+++ js/header/nav/mobile/close_nav.js
@@ -1,9 +1,22 @@
 /*importamos los enlaces del DOM */
+import { link } from "../desktop/remove_border_link.js";
+/*También importamos el menu */
 import { nav_list } from "./show_nav_list_responsive.js";
-// import { link } from "../desktop/remove_border_link.js";
+/*importar el método que transforma el icono hamburguesa */
+import { rotateBar } from "./rotate_ham_burger.js";
 
 const closeNav = () => {
-  nav_list.classList.remove("nav__list--activate");
+  /*Por cada link dentro del menú se usa el parámetro link_item */
+  link.forEach((link_item) => {
+    //Este parámetro al escuchar el evento click se ejecutará una  función
+    link_item.addEventListener("click", function () {
+      /*Mediante la propiedad classList se quitarán las clases mostrar el menú y ocultar el scroll del body */
+      nav_list.classList.remove("nav__list--activate");
+      body.classList.remove("scroll-hidden");
+      /*Además, se ejecutará la función para rotar las barras y no se quede como el símbolo de borrar */
+      rotateBar();
+    });
+  });
 };
 
 export { closeNav };
--- js/main.js
+++ js/main.js
@@ -3,7 +3,8 @@ import { showHiddenHeader } from "./header/nav/desktop/show_hide_nav.js";
 import { rotateImage } from "./main/projects/card/desktop/rotate_img.js";
 import { linkButton } from "./main/projects/card/desktop/link_button.js";
 import { linkContact } from "./main/contact/desktop/link_contact.js";
-import { activaLink } from "./header/nav/desktop/activate_link.js";
+import { activateLink } from "./header/nav/desktop/activate_link.js";
+import { closeNav } from "./header/nav/mobile/close_nav.js";
 // Abrir y cerrar el botón con forma de hamburguesa en vista para móviles.
 openCloseHam();
 /*Mostrar el header al hacer un scroll hacia arriba y ocultarlo cuando el scroll es hacia abajo */
@@ -12,6 +13,9 @@ showHiddenHeader();
 rotateImage();
 /*Dar enlace a los botones de los cards */
 linkButton();
+/*Ejecutar función que permite abrir los enlaces de cada red social almacenada en un botón */
 linkContact();
-
-activaLink();
+/*Dar subrayado a los enlaces cuando está en su sección correspondiente */
+activateLink();
+/*Cerrar ventana de menu en modo responsive */
+closeNav();
